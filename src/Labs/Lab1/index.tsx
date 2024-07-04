7ea9e0420c7c6067164c54599a401b14b05b6d55
nnaim36 <Nnaim36@gmail.com>
2024-07-04 18:54:10 -0400

    up to text fields

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -62,6 +62,64 @@ tag to tell browsers to render the gaps.
                     <li>Devil in the white castle</li>
                     <li>Pokemon Game guide</li>
                 </ul>
+                <div id="wd-tables">
+                    <h4>Table Tag</h4>
+                    <table border={1} width="100%">
+                        <thead>
+                            <tr>
+                                <th>Quiz</th>
+                                <th>Topic</th>
+                                <th>Date</th>
+                                <th>Grade</th>
+                            </tr>    
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Q1</td>
+                                <td>HTML</td>
+                                <td>2/3/21</td>
+                                <td>85</td>
+                            </tr>
+                            <tr>
+                                <td>Q2</td>
+                                <td>CSS</td>
+                                <td>2/10/21</td>
+                                <td>90</td>
+                            </tr> 
+                            <tr>
+                                <td>Q3</td>
+                                <td>JavaScript</td>
+                                <td>2/17/21</td>
+                                <td>95</td>
+                            </tr>  
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td colSpan={3}>Averade</td>
+                                <td>90</td>
+                            </tr>
+                        </tfoot>       
+                    </table>
+                </div>
+                <div id="wd-images">
+                    <h4>Image tag</h4>
+                    Loading an image from the internet:
+                    <br />
+                    <img id="wd-starship"
+                    width="400px" 
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" 
+                    />
+                    <br />
+                    Loading a local image:
+                    <br />
+                    <img src="images/teslabot.png" height="200px"/>
+                </div>
+                <div id="wd-forms">
+                    <h4>Form Elements</h4>
+                    <form id="wd-text-fields">
+                        <h5>Text Fields</h5>
+                    </form>
+                </div>
             </div>
         </div>
     );
