3ab62ac62d107f3287bbeac0ec8f131ca427425d
Nile Naim <nnaim36@gmail.com>
2024-07-08 17:44:59 -0400

    added github link and name to lab

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -6,6 +6,7 @@ export default function TOC() {
         <li><a id="wd-a2" href="#/Labs/Lab2">Lab 2</a></li>
         <li><a id="wd-a3" href="#/Labs/Lab3">Lab 3</a></li>
         <li><a id="wd-k"  href="#/Kanbas">Kanbas</a></li>
+        <li><a id="wd-github" href="https://github.com/nnaim36/kanbas-react-web-app-2024-su2">Github</a></li>
       </ul>
     );
   }
